0dbac531e87bd7ef8e29a4a507d88877198ffb65
wpaoli <wpaoli@gmail.com>
2023-11-21 16:33:01 -0800

    Just the basic table printing out

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import JobsTable from "./JobsTable";
-import EnterJob from "./EnterJob";
 import axios from "axios";
 
 const App = () => {
@@ -11,70 +9,45 @@ const App = () => {
   useEffect(() => {
     axios
       .get("http://localhost:3000/jobs")
-      .then((response) => setJobs(response.data))
+      .then((response) => {
+        setJobs(response.data);
+      })
       .catch((error) =>
         console.error(`There was an error retrieving the job list: ${error}`)
       );
   }, []);
 
-  //Modifying the list of jobs to add a key for react
-  const dataSource = jobs.map((job) => ({
-    ...job,
-    key: job.JobID,
-  }));
-
-  //Function to Update State of jobs, this gets fired from the 'onJobUpdate'
-  //that comes back from from the EnterJob.jsx
-  //Im still sorta unclear on how this actually works, which fucking pisses me off.
-  //TODO: Create a super basic example of this function attribute stuff.
-
-  const addJob = (job) => {
-    setJobs((prevJobs) => [...prevJobs, job]);
-    setConfirmLoading(true);
-    setTimeout(() => {
-      setOpen(false);
-      setConfirmLoading(false);
-    }, 2000);
-  };
-  const [open, setOpen] = useState(false);
-  const [confirmLoading, setConfirmLoading] = useState(false);
-  const [form, setForm] = useState(null);
-
-  const showModal = () => {
-    setOpen(true);
-  };
-
-  const handleCancel = () => {
-    console.log("Clicked cancel button");
-    setOpen(false);
-  };
-
-  const handleOk = () => {
-    console.log(form);
-    if (form) {
-      form.submit();
-      console.log(form);
-    }
+  const JobsTable = ({ jobs }) => {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th>Job Title</th>
+            <th>Link to Job Posting</th>
+            <th>Date Applied</th>
+            <th>Notes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {jobs.map((job) => (
+            <tr key={job.JobId}>
+              <td>{job.Company}</td>
+              <td>{job.JobTitle}</td>
+              <td>{job.JobPosting}</td>
+              <td>{job.DateApplied}</td>
+              <td>{job.Notes}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
   };
 
   return (
-    <div>
-      <JobsTable jobs={dataSource} />
-      {console.log({ dataSource })}
-      <Button type="primary" onClick={showModal}>
-        Add Job
-      </Button>
-      <Modal
-        title="Add New Job"
-        open={open}
-        confirmLoading={confirmLoading}
-        onCancel={handleCancel}
-        okText="Submit"
-        onOk={handleOk}
-      >
-        <EnterJob onJobUpdate={addJob} formInstance={setForm} />
-      </Modal>
-    </div>
+    <>
+      <JobsTable jobs={jobs} />
+    </>
   );
 };
 
